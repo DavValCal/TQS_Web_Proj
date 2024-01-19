94a94b62f489bcc86fabc0e2d040d1593bf310a7
jheremmyMatienzo02 <104379940+jheremmyMatienzo02@users.noreply.github.com>
2024-01-19 20:42:05 +0100

    Delete segonaPractica directory

--- segonaPractica/src/test/java/RunAllTest.java
+++
@@ -1,8 +0,0 @@
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features="src/test/java/features", glue="steps")
-public class RunAllTest extends AbstractTestNGCucumberTests{
-	
-}
-// cuidado con el orden de ejecucion de features
