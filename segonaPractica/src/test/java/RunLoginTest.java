31595511d059d5584586bd9ec9ab9018c07f4471
jheremmyMatienzo02 <104379940+jheremmyMatienzo02@users.noreply.github.com>
2024-01-21 15:50:06 +0100

    Delete segonaPractica/src/test/java/RunLoginTest.java

--- segonaPractica/src/test/java/RunLoginTest.java
+++
@@ -1,7 +0,0 @@
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features="src/test/java/features/1_login.feature", glue="steps")
-public class RunLoginTest extends AbstractTestNGCucumberTests{
-	
-}
