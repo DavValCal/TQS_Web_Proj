cf5832a864771e01b81c804e4da47f91c3b71988
jheremmyMatienzo02 <104379940+jheremmyMatienzo02@users.noreply.github.com>
2024-01-21 15:50:23 +0100

    Delete segonaPractica/src/test/java/RunCoursesTest.java

--- segonaPractica/src/test/java/RunCoursesTest.java
+++
@@ -1,7 +0,0 @@
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features="src/test/java/features/courses.feature", glue="steps")
-public class RunCoursesTest extends AbstractTestNGCucumberTests{
-
-}
