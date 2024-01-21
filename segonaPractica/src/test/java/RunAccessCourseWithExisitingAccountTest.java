c26ba9627d877a1093883b59ff5660a013eab5da
jheremmyMatienzo02 <104379940+jheremmyMatienzo02@users.noreply.github.com>
2024-01-21 15:49:17 +0100

    Delete segonaPractica/src/test/java/RunAccessCourseWithExisitingAccountTest.java

--- segonaPractica/src/test/java/RunAccessCourseWithExisitingAccountTest.java
+++
@@ -1,7 +0,0 @@
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features="src/test/java/features/o_accessCourseWithExistingAccount.feature", glue="steps")
-public class RunAccessCourseWithExisitingAccountTest extends AbstractTestNGCucumberTests{
-
-}
