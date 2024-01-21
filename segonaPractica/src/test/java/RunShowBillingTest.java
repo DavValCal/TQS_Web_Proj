e5785154c6c7f0fb5a4123c96c17c1c8291324be
jheremmyMatienzo02 <104379940+jheremmyMatienzo02@users.noreply.github.com>
2024-01-21 15:49:31 +0100

    Delete segonaPractica/src/test/java/RunShowBillingTest.java

--- segonaPractica/src/test/java/RunShowBillingTest.java
+++
@@ -1,7 +0,0 @@
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features="src/test/java/features/showBilling.feature", glue="steps")
-public class RunShowBillingTest extends AbstractTestNGCucumberTests{
-	
-}
